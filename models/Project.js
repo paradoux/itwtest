32ed94ff2f22e6beb0eb550259160512285c561b
Axel Martin <martin.axe@live.fr>
2018-07-25 11:12:40 +0200

    Reset Database with clean ids

--- models/Project.js
+++ models/Project.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const projectSchema = new Schema({
-    _id: { type: String, required: true, unique: true },
+    _id: { type: String, required: true },
     title: { type: String, required: true },
     description: { type: String, required: true },
     phase: { type: String, required: true },
